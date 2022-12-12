3250873f0a1f19db92ed8c3fdde1848ff8659389
Lucas Mendelowski <lucas@mendelowski.com>
2022-12-12 21:47:37 +0100

    Fix gopls install

--- lua/lspinstall/servers/gopls.lua
+++ lua/lspinstall/servers/gopls.lua
@@ -1,7 +1,7 @@
 return {
   cmd = { "./gopls" },
   install_script = [[
-  GOPATH=$(pwd) GOBIN=$(pwd) GO111MODULE=on go get -v golang.org/x/tools/gopls
-  GOPATH=$(pwd) GO111MODULE=on go clean -modcache
+  GOPATH=$(pwd) GOBIN=$(pwd) go install -v golang.org/x/tools/gopls@latest
+  GOPATH=$(pwd) go clean -modcache
   ]]
 }
